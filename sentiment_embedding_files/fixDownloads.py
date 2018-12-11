fb4994991ea334295d02d102dc067feb714a64b5
David Branse <dbranse027@gmail.com>
2018-12-10 23:42:31 -0500

    changed tweet format for sswe: pls work

---
+++ sentiment_embedding_files/fixDownloads.py
@@ -0,0 +1,8 @@
+import string
+
+formatting = '{SID}\t{UID}\t{emote}\t{message}\n'
+with open('downloaded_tweets.tsv', 'r') as tweets:
+	with open('tweets.tsv', 'a') as file:
+		for line in tweets:
+			s = line.split('\t')
+			file.write(formatting.format(SID=s[0], UID=s[1], emote=s[4], message=s[5]))
