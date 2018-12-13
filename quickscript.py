7bed5afb88bfe837a51f6699e504b04a0a953584
marko-fejzo <markos.business@gmail.com>
2018-12-13 02:05:40 -0500

    added quik

---
+++ quickscript.py
@@ -0,0 +1,19 @@
+import csv
+with open('newLabeledTrain.txt', 'r') as tsv: #, open('newLabeledTrain.txt', 'w') as tsv2:
+  c = csv.DictReader(tsv, delimiter='\t')
+  m = 0
+  ls = []
+  a = []
+  for i in c:
+    if i['turn1'] is None or i['turn2'] is None or i['turn3'] is None:
+      continue
+    k = len(i['turn1'].split()) + len(i['turn2'].split()) + len(i['turn3'].split()) + 2
+    m = max(m, k)
+    ls.append(k)
+    if k <= 100:
+      a.append(i)
+  print(m)
+  print(sum(ls)/len(ls))
+  # dict_writer = csv.DictWriter(tsv2, a[0].keys(), delimiter='\t')
+  # dict_writer.writeheader()
+  # dict_writer.writerows(a)
